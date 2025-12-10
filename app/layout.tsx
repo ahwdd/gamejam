fe822609606c1b9eb4af19a9d492208727d5cc15
ranim-99 <ranim.1999.ta@gmail.com>
2025-12-10 18:52:04 +0200

    temp remove store

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import StoreProvider from "@/components/StoreProvider";
+// import StoreProvider from "@/components/StoreProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen overflow-x-hidden min-h-screen`}>
-        <StoreProvider>
+        {/* <StoreProvider> */}
           {children}
-        </StoreProvider>
+        {/* </StoreProvider> */}
       </body>
     </html>
   );
